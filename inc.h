354f3e6ccd4a97e63e146f32008722a4e254eece
bagdades <volodumurkoval0@gmail.com>
2017-10-27 16:28:24 +0300

    Start projects

---
+++ inc.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  inc.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  27.10.17 15:26:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vovan (), volodumurkoval0@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  27.10.17 14:28:43
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vovan (), volodumurkoval0@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "inc.h"
+
+int main(void)
+{
+	while(1);
+	return 0;
+}
